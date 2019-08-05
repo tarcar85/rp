938c95733d6c0a8c7ef66c4b7e99851d5664619f
secobau <sebastian.colomar@gmail.com>
2019-08-05 17:51:17 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -44,8 +44,9 @@ for x in $all ;
 for x in api ;
  do
   ssh $x ' curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash && . ~/.nvm/nvm.sh && nvm install 10 ' ;
-  ssh $x ' sudo yum install git -y && git clone https://github.com/secobau/nodejs && cd nodejs/rest-api ' ;
-  ssh $x ' rm package.json && npm init --yes && npm install express && sed -i /app.listen/d index.js && echo app.listen(8080) 1>>index.js ' ;
+  ssh $x ' sudo yum install git -y && git clone https://github.com/secobau/nodejs ' ;
+  ssh $x ' cd nodejs/rest-api && rm package.json && npm init --yes && npm install express ' ;
+  ssh $x ' file=nodejs/rest-api/index.js  && sed -i /app.listen/d $file && echo app.listen(8080) 1>>$file ' ;
  done ;
 
 for x in api ;
