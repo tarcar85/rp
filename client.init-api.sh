88877eb6f0fbfd240aec8f029ecd006f928b9e6b
secobau <sebastian.colomar@gmail.com>
2019-08-05 16:41:38 +0200

    Create client.init-api.sh

---
+++ client.init-api.sh
@@ -0,0 +1,3 @@
+curl -H "Content-Type: application/json" -X POST -d '{"colour":"red", "size":"medium"}'  "http://192.168.255.40/accounts"
+curl -H "Content-Type: application/json" -X POST -d '{"colour":"red", "size":"medium"}'  "http://192.168.255.40/accounts"
+curl -H "Content-Type: application/json" -X POST -d '{"colour":"red", "size":"medium"}'  "http://192.168.255.40/accounts"
