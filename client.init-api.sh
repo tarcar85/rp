231dd175b99751bce1933489b1e35ca84c1a2bab
secobau <sebastian.colomar@gmail.com>
2019-08-07 13:12:02 +0200

    Update client.init-api.sh

--- client.init-api.sh
+++ client.init-api.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -x
+#!/bin/bash
 curl -H "Content-Type: application/json" -X POST -d '{"colour":"red", "size":"small"}'  "http://rp/accounts"
 curl -H "Content-Type: application/json" -X POST -d '{"colour":"green", "size":"medium"}'  "http://rp/accounts"
 curl -H "Content-Type: application/json" -X POST -d '{"colour":"blue", "size":"large"}'  "http://rp/accounts"
+curl rp/accounts;echo
